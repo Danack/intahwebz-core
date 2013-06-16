c2026ff8024a59528cd4da1f57e4152414976249
Danack <Danack@basereality.com>
2013-06-16 19:27:18 +0100

    Made interface into class - probably a bad idea long term. Abstract class should be in a project.

--- src/Intahwebz/ImageFile.php
+++ src/Intahwebz/ImageFile.php
@@ -4,11 +4,70 @@
 namespace Intahwebz;
 
 
-interface ImageFile {
+abstract class ImageFile {
 
-	function saveImage($destFileName);
+	public $srcWidth, $srcHeight;
+	public $destWidth, $destHeight;
 
-	function setResize($resizeParam);
+	abstract function saveImage($destFileName);
 
-	function setWidthHeight($maxWidth, $maxHeight);
+	function setWidthHeightFromWidth($newWidth) {
+		$newHeight = ($newWidth * $this->srcHeight) / $this->srcWidth;
+		$this->destWidth = intval(ceil($newWidth));
+		$this->destHeight = intval(ceil($newHeight));
+	}
+
+	function setWidthHeightFromHeight($newHeight) {
+		$newWidth = ($newHeight * $this->srcHeight) / $this->srcWidth;
+		$this->destWidth = intval(ceil($newWidth));
+		$this->destHeight = intval(ceil($newHeight));
+	}
+
+	function setWidthHeightFromMaxDimensions($maxWidth, $maxHeight) {
+		$ratio = $this->srcWidth / $this->srcHeight;
+
+		if($this->srcWidth > $maxWidth || $this->srcHeight > $maxHeight){
+			$newWidth = $maxWidth;
+			$newHeight = $newWidth / $ratio;
+
+			if($newHeight > $maxHeight){
+				$newHeight  = $maxHeight;
+				$newWidth = $maxHeight * $ratio;
+			}
+		}
+
+		$newWidth = intval(ceil($newWidth));
+		$newHeight = intval(ceil($newHeight));
+
+		$this->destWidth = $newWidth;
+		$this->destHeight = $newHeight;
+	}
+
+	function setResize($resizeParam){
+		if($resizeParam == 'thumbnail' || $resizeParam == 'thumb'){
+			$size = THUMBNAIL_SIZE;
+			$this->setWidthHeightFromMaxDimensions(THUMBNAIL_SIZE, THUMBNAIL_SIZE);
+		}
+
+		if ($size == 'original') {
+			//TODO - Avoid resize
+			$this->destWidth = $this->srcWidth;
+			$this->destHeight = $this->srcHeight;;
+			return;
+		}
+
+		$pattern = '#(\d+)(\w?)#u';
+
+		$matches = preg_match($pattern, $resizeParam, $match);
+
+		if ($matches == 0) {
+			//Match failed, just set Thumbnail size
+			$this->setWidthHeightFromMaxDimensions(THUMBNAIL_SIZE, THUMBNAIL_SIZE);
+		}
+
+		var_dump($matches);
+
+		return $size;
+	}
+
 }
