1676746bfeefe6ab541a1596e7a4cee2b858b265
Danack <Danack@basereality.com>
2013-06-16 17:36:16 +0100

    Added type hinting.

--- src/Intahwebz/ImageLoader.php
+++ src/Intahwebz/ImageLoader.php
@@ -3,10 +3,20 @@
 
 namespace Intahwebz;
 
+use Intahwebz\ImageFile;
 
 interface ImageLoader{
 
+	/**
+	 * @param $srcFileName
+	 * @return ImageFile
+	 */
 	function createImageFromFile($srcFileName);
+
+	/**
+	 * @param $blob
+	 * @return ImageFile
+	 */
 	function createImageFromBlob($blob);
 
 }
