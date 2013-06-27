cb27e33245a3aee81be36a3c2ac9a639007a1cbb
Danack <Danack@basereality.com>
2013-06-27 16:53:29 +0100

    Removed static from calls as they are no longer static.

--- src/Intahwebz/Router.php
+++ src/Intahwebz/Router.php
@@ -6,13 +6,13 @@ namespace Intahwebz;
 
 interface Router {
 
-	static function	generateCurrentURL($parameters, $absolute = FALSE);
+	function	generateCurrentURL($parameters, $absolute = FALSE);
 
-	static function generateURLForRoute($routeName, $parameters = array(), $absolute = FALSE);
+	function generateURLForRoute($routeName, $parameters = array(), $absolute = FALSE);
 
-	static function forward($routeName, $parameters = array(), $absolute = FALSE);
+	function forward($routeName, $parameters = array(), $absolute = FALSE);
 
-	static function forwardToRoute($route, $parameters, $absolute = false);
+	function forwardToRoute($route, $parameters, $absolute = false);
 
 	//Todo - probably shouldn't need to pass in the view
 	function	reRouteRequest($routeName, $view);
