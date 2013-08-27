13338570cd5397e6966972112347057656c10e9b
Danack <Danack@basereality.com>
2013-08-28 00:28:42 +0100

    Added exception check for bad parameter.

--- src/Intahwebz/Path.php
+++ src/Intahwebz/Path.php
@@ -9,6 +9,9 @@ class Path {
     private $path;
 
     function __construct($string) {
+        if ($string == null) {
+            throw new \Exception("Path cannot be null for class ".get_class($this));
+        }
         $this->path = $string;
     }
 
