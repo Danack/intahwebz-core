623b685d96f8d708709be51e5ca7e009231950d6
Danack <Danack@basereality.com>
2013-09-15 14:12:18 +0100

    Refactor in progress.

--- src/Intahwebz/Functions.php
+++ src/Intahwebz/Functions.php
@@ -2,7 +2,6 @@
 
 namespace Intahwebz {
 
-
     class Functions {
         public static function load() {
         }
@@ -12,6 +11,11 @@ namespace Intahwebz {
 
 
 namespace { // global code
+
+    use Intahwebz\Utils\Utils;
+    use Intahwebz\Utils\RemoteDownloadedFile;
+    use Intahwebz\Utils\UserUploadedFile;
+    use Intahwebz\Utils\FileUploadException;
 
     function getLastModifiedTime($timestamp) {
         return gmdate('D, d M Y H:i:s', $timestamp). ' UTC';
@@ -249,5 +253,141 @@ namespace { // global code
 
         return date($format, $unixTimestamp);
     }
+
+    /**
+     * @param $imageURL
+     * @return RemoteDownloadedFile
+     */
+    function	getImageFromLink($imageURL){
+        $tempFilename = tempnam(sys_get_temp_dir(), 'Tux');
+
+        $fileHandle = fopen($tempFilename, 'w+');
+
+        $headerArray = curlDownloadFileAndReturnHeaders($imageURL, $fileHandle);
+
+        $urlInfo = parse_url($imageURL);
+
+        $fileInfo = array();
+
+        foreach($headerArray as $headerKey => $headerValue){
+            if(strcasecmp('Content-type', $headerKey) == 0 ||
+                strcasecmp('content_type', $headerKey) == 0){
+                $fileInfo['contentType'] = $headerValue;
+            }
+        }
+
+        $lastSlashPosition = strrpos($urlInfo['path'], '/');
+
+        if($lastSlashPosition === FALSE){
+            $filename = $urlInfo['path'];
+        }
+        else{
+            $filename = substr($urlInfo['path'], $lastSlashPosition + 1);//+1 to exclude the slash
+        }
+
+        if(strlen($filename) == 0){
+            $filename = date("Y_m_d_H_i_s");
+            //Cannot guess image type from made up file name.
+
+            if(array_key_exists('contentType', $fileInfo) == TRUE){
+                $extension = Utils::getFileExtensionForMimeType($fileInfo['contentType']);
+                $filename .= ".".$extension;
+            }
+        }
+
+        fclose($fileHandle);
+
+        return new RemoteDownloadedFile(
+            $filename,
+            $tempFilename,
+            filesize($tempFilename)
+        );
+    }
+
+    function getNormalizedFILES(){
+        $newFiles = array();
+        foreach($_FILES as $fieldName => $fieldValue){
+            foreach($fieldValue as $paramName => $paramValue){
+                foreach((array)$paramValue as $index => $value){
+                    $newFiles[$fieldName][$paramName] = $value;
+                }
+            }
+        }
+        return $newFiles;
+    }
+
+
+    /**
+     * @param $formFileName
+     * @return UserUploadedFile
+     * @throws FileUploadException
+     * @throws Exception
+     */
+    function getUserUploadedFile($formFileName){
+        //logToFileDebug("getUploadedFileInfo");
+
+        $files = getNormalizedFILES();
+
+        if(isset($files[$formFileName]) == FALSE){
+            //logToFileDebug_var($files);
+            //throw new Exception("File not uploaded. \$files[".$formFileName."] is not set.");
+            return false;
+        }
+        else{
+            if($files[$formFileName]['error'] == UPLOAD_ERR_OK) {
+                if(is_uploaded_file($files[$formFileName]['tmp_name']) ){
+                    //logToFileDebug("File [$formFileName] looks valid details are ".getVar_DumpOutput($files[$formFileName]));
+
+                    return new UserUploadedFile(
+                        $files[$formFileName]['name'],
+                        $files[$formFileName]['tmp_name'],
+                        $files[$formFileName]['size']
+                    );
+                }
+                else{
+                    throw new FileUploadException("File not uploaded. Status [".$files[$formFileName]['error']."] indicated error.");
+                }
+            }
+            else{
+                //var_dump($files);
+                throw new FileUploadException("Error detected in upload: ".getFileUploadErrorMeaning($files[$formFileName]['error']));
+            }
+        }
+    }
+
+
+
+
+    function getFileUploadErrorMeaning($errorCode){
+
+        switch($errorCode){
+            case (UPLOAD_ERR_OK):{ //no error; possible file attack!
+                return "There was a problem with your upload.";
+            }
+            case (UPLOAD_ERR_INI_SIZE): {//uploaded file exceeds the upload_max_filesize directive in php.ini
+                return "The file you are trying to upload is too big.";
+            }
+            case (UPLOAD_ERR_FORM_SIZE):{ //uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the html form
+                return "The file you are trying to upload is too big.";
+            }
+            case UPLOAD_ERR_PARTIAL: {//uploaded file was only partially uploaded
+                //Todo - allow partial uploads
+                return 	"The file you are trying upload was only partially uploaded.";
+            }
+            case (UPLOAD_ERR_NO_FILE): {//no file was uploaded
+                return 	"You must select a file for upload.";
+            }
+
+            //TODO - handle these
+//			UPLOAD_ERR_NO_TMP_DIR
+//			UPLOAD_ERR_CANT_WRITE
+//			UPLOAD_ERR_EXTENSION
+
+            default: {	//a default error, just in case!  :)
+            return	"There was a problem with your upload, error code is ".$errorCode;
+            }
+        }
+    }
+
 
 }
