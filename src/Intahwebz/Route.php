04f4243b8d3b4c801586be62a6292bc4b36f7ca9
Danack <Danack@basereality.com>
2014-01-23 22:10:52 +0000

    Tidied up interface by removing unneeded functions.

--- src/Intahwebz/Route.php
+++ src/Intahwebz/Route.php
@@ -12,15 +12,11 @@ interface Route {
     public function getCallable();
 
     public function generateURL(\Intahwebz\Domain $domain, $parameters, $absolute = false);
-    public function mapParametersToFunctionArguments(Request $request);
-    public function matchRequestAndStoreParams(Request $request);
+    public function matchRequest(Request $request);
 
     public function getTemplate();
 
     public function getMergedParameters(Request $request, $params);
-
-//    public function getRouteParam($name);
-//    public function getRouteParams();
 
     public function getACLPrivilegeName();
     public function getACLResourceName();
